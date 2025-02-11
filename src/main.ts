56530c9e144fe34913f049aa8e6ff35f4a516727
pmorais13 <phmscout@gmail.com>
2025-02-10 21:41:11 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitar CORS para permitir requisições de um domínio específico
+  app.enableCors({
+    origin: 'http://localhost:4200/', // Substitua pelo domínio do seu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
